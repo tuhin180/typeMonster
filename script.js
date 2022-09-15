548a4e1f9f3a96a50cac47034075b7ae1591a4d7
tuhin180 <tanjirtuhin18@gmail.com>
2022-09-15 18:04:57 +0600

    finishing touch

--- script.js
+++ script.js
@@ -47,10 +47,10 @@ const typeController = (e) => {
       newLetter === " " ? "▪" : newLetter
     }</span>`;
   } else {
+    errorCount++;
     display.innerHTML += `<span class="red">${
       newLetter === " " ? "▪" : newLetter
     }</span>`;
-    errorCount++;
   }
 
   // check if given question text is equal to user typed text
