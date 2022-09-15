2704820709b960b9eaa0d30c71454fc79e8c7cb2
tuhin180 <tanjirtuhin18@gmail.com>
2022-09-15 08:38:38 +0600

    error is counting

--- script.js
+++ script.js
@@ -50,6 +50,7 @@ const typeController = (e) => {
     display.innerHTML += `<span class="red">${
       newLetter === " " ? "▪" : newLetter
     }</span>`;
+    errorCount++;
   }
 
   // check if given question text is equal to user typed text
